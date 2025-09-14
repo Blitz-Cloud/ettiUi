6f6eb2ea314ffed9ea68c1e52e4908c992654955
BlitzCloud <ionutp626@gmail.com>
2025-09-14 21:52:21 +0300

    Update added light theme

--- app/root.tsx
+++ app/root.tsx
@@ -85,7 +85,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="">
         <MsalProvider instance={pca}>{children}</MsalProvider>
         <ScrollRestoration />
         <Scripts />
